19dc73a2f2d99456160a2aa278c5d98e36a7e0c3
jvouillon <jeanvouillon@MacBook-Pro-de-jean.local>
2016-10-04 15:38:42 -0500

    t

--- CM215/00/sketch.js
+++ CM215/00/sketch.js
@@ -13,9 +13,9 @@ function draw() {
 
 function touchStarted() {
   textSize(40);
-  text(touches[0].id, 100, posY);
-  text(touches[0].x, 200, posY);
-  text(touches[0].y, 300, posY);
+  text(touches[0].id, 50, posY);
+  text(touches[0].x, 500, posY);
+  text(touches[0].y, 600, posY);
   posY += 50;
   
 }
