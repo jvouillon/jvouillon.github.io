823b1b7110ffbf108cc25d00cf2ac4a3f567e052
jvouillon <jeanvouillon@MacBook-Pro-de-jean.local>
2016-10-04 15:12:28 -0500

    touch

--- CM215/00/sketch.js
+++ CM215/00/sketch.js
@@ -1,5 +1,7 @@
 function setup() {
  createCanvas(windowWidth,windowHeight); 
+ 
+ var posY = 50;
 }
 
 function draw() {
@@ -9,6 +11,8 @@ function draw() {
 function touchStarted() {
   textSize(32);
   
-  text(touches[0].x, 100, 50);
+  text(touches[0].x, 100, posY);
+  text(touches[0].y, 200, posY);
+  posY += 50;
   
 }
