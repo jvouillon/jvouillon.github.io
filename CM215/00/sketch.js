bdf3e15df3aa341935cc3ee1e931da8342f93232
jvouillon <jeanvouillon@MacBook-Pro-de-jean.local>
2016-10-05 09:32:55 -0500

    t

--- CM215/00/sketch.js
+++ CM215/00/sketch.js
@@ -1,7 +1,6 @@
  function setup() {
  createCanvas(windowWidth,windowHeight);
  rectMode(CENTER);
- fill(255,0,0);
 }
 
 function draw()
@@ -9,5 +8,6 @@ function draw()
 
 function touchStarted() {
  background(255);
+ fill(random(0,255),random(0,255),random(0,255));
  rect(touches[0].x,touches[0].y,150,150);
 }
