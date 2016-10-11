a96e69823bb2a379cda56a1522045b3fd8f869b2
jvouillon <jeanvouillon@mbp-de-jean.gateway.mts.net>
2016-10-11 14:58:30 -0500

    c

--- CM215/00/sketch.js
+++ CM215/00/sketch.js
@@ -1,29 +1,35 @@
-
-document.ontouchmove = function(event){
+document.ontouchmove = function(event) {
   event.preventDefault();
 }
 
 function setup() {
- createCanvas(windowWidth,windowHeight);
- rectMode(CENTER);
- stroke(0);
- fill(255,0,0,0.5);
- strokeWeight(5);
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  stroke(0);
+  fill(255, 0, 0, 0.5);
+  strokeWeight(5);
 }
 
-function draw()
-{
+function draw() {
   background(255);
- 
- for(var i=0;i<touches.length;i++){
-   rect(touches[i].x,touches[i].y,150,150);
-   
-   if((i+1)<touches.length){
-     line(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
-   }else{
-     line(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
-   }
- }
+  beginShape();
+  for (var i = 0; i < touches.length; i++) {
+    vertex(touches[i].x, touches[i].y);
+  }
+  endShape()
+
+
+
+  for (var i = 0; i < touches.length; i++) {
+    rect(touches[i].x, touches[i].y, 150, 150);
+
+    if ((i + 1) < touches.length) {
+      line(touches[i].x, touches[i].y, touches[i + 1].x, touches[i + 1].y);
+    } else {
+      line(touches[i].x, touches[i].y, touches[0].x, touches[0].y);
+    }
+  }
+
 }
 
 function touchStarted() {
