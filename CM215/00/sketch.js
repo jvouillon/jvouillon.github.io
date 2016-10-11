c3727e14d5e7148e5957136c4e4f453b9be04440
jvouillon <jeanvouillon@mbp-de-jean.gateway.mts.net>
2016-10-11 15:00:50 -0500

    c

--- CM215/00/sketch.js
+++ CM215/00/sketch.js
@@ -12,11 +12,12 @@ function setup() {
 
 function draw() {
   background(255);
+
   beginShape();
   for (var i = 0; i < touches.length; i++) {
     vertex(touches[i].x, touches[i].y);
   }
-  endShape()
+  endShape(CLOSE)
 
 
 
