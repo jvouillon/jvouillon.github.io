8b761adf5de9d60b9b8aea05a5090b55e1055434
jvouillon <jeanvouillon@mbp-de-jean.gateway.mts.net>
2016-10-12 15:50:47 -0500

    01

--- CM215/01/sketch.js
+++ CM215/01/sketch.js
@@ -1,73 +1,74 @@
-
 // Position Variables
 var x = 0;
 var y = 0;
- 
+
 // Speed - Velocity
 var vx = 0;
 var vy = 0;
- 
+
 // Acceleration
 var ax = 0;
 var ay = 0;
- 
-var vMultiplier = 0.01; 
+
+var vMultiplier = 0.01;
 var bMultiplier = 0.6;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    x = windowWidth / 2;
-    y = windowHeight / 2;
-    
+  createCanvas(windowWidth, windowHeight);
+  x = windowWidth / 2;
+  y = windowHeight / 2;
+
 }
 
 function draw() {
-    background(255);
-    fill(0);
-    textSize(50);
-    text("Rx: " + round(rotationX), 20,20);
-    text("Ry: " + round(rotationY), 20,80);
-    text("Rz: " + round(rotationZ), 20,140);
-    
-    ballMove();
-    
-    noStroke();
-    fill(60);
-    ellipse(x, y+5, 60, 60);
-    fill(150);
-    ellipse(x, y, 60, 60);
-    fill(255);
-    ellipse(x+5, y-17, 20, 20);
+  background(255);
+
+  ballMove();
+
+  noStroke();
+  fill(60);
+  ellipse(x, y + 5, 60, 60);
+  fill(150);
+  ellipse(x, y, 60, 60);
+  fill(255);
+  ellipse(x + 5, y - 17, 20, 20);
 }
 
 function ballMove() {
 
   ax = rotationY * vMultiplier;
-	ay = rotationX * vMultiplier;
-	
-	vx += ax;
-	vy += ay;
-	
-	y = y + vy; 
-	x = x + vx;
+  ay = rotationX * vMultiplier;
 
-	// Bounce when touch the edge of the canvas
-	if (x < 0) { 
-		x = 0; 
-		vx = -vx * bMultiplier; 
-	}
- 	if (y < 0) { 
- 		y = 0; 
- 		vy = -vy * bMultiplier; 
- 	}
- 	if (x > width - 20) { 
- 		x = width - 20; 
- 		vx = -vx * bMultiplier; 
- 	}
- 	if (y > height - 20) { 
- 		y = height - 20; 
- 		vy = -vy * bMultiplier; 
- 	}
-	
+  vx += ax;
+  vy += ay;
+
+  y = y + vy;
+  x = x + vx;
+
+  // Bounce when touch the edge of the canvas
+  if (x < 0) {
+    x = 0;
+    vx = -vx * bMultiplier;
+  }
+  if (y < 0) {
+    y = 0;
+    vy = -vy * bMultiplier;
+  }
+  if (x > width - 20) {
+    x = width - 20;
+    vx = -vx * bMultiplier;
+  }
+  if (y > height - 20) {
+    y = height - 20;
+    vy = -vy * bMultiplier;
+  }
+
 }
 
+function deviceMoved() {
+  fill(0);
+  textSize(50);
+  text("Rx: " + round(rotationX), 20, 20);
+  text("Ry: " + round(rotationY), 20, 80);
+  text("Rz: " + round(rotationZ), 20, 140);
+}
