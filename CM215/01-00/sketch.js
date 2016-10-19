5ca769314325be8718590975fec9a0014f8f42d6
jvouillon <jeanvouillon@MacBook-Pro-de-jean.local>
2016-10-19 10:42:34 -0500

    class

---
+++ CM215/01-00/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+ createCanvas(windowWidth,windowHeight); 
+}
+
+function draw() {
+ background(255);
+ textSize(40);
+ text("Rx: " + floor(rotationX), 100, 100);
+ text("Ry: " + floor(rotationY), 100, 150);
+ text("Rz: " + floor(rotationZ), 100, 200);
+}
