aed03e6ffc2dd4e65b9c643449cdcf4d9f38a807
jvouillon <jeanvouillon@MacBook-Pro-de-jean.local>
2016-10-19 11:13:17 -0500

    00

--- CM215/01-00/sketch.js
+++ CM215/01-00/sketch.js
@@ -11,15 +11,14 @@ function draw() {
   background(255);
   
   drawBall();
-  
   textSize(40);
   
-  //text("Rx: " + floor(rotationX), 100, 100);
-  //text("Ry: " + floor(rotationY), 100, 150);
-  //text("Rz: " + floor(rotationZ), 100, 200);
+  text("Rx: " + floor(rotationX), 100, 100);
+  text("Ry: " + floor(rotationY), 100, 150);
+  text("Rz: " + floor(rotationZ), 100, 200);
 }
 
 function drawBall(){
   fill(100);
-  ellipse(windowWidth/2,windowHeight/2, ballSize, ballSize);
+  ellipse(windowWidth/2,rotationX+windowHeight/2, ballSize, ballSize);
 }
