51ff1bb5c68b32810c87ff74042cd6926d00859e
jvouillon <jeanvouillon@MacBook-Pro-de-jean.local>
2017-01-05 09:52:42 -0600

    p5

---
+++ CM127/01/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(800,600);
+  
+}
+
+function draw() {
+  fill(255,0,0);
+  strokeWeight(10);
+  stroke(0,0,255);
+  ellipse(100,100,100,100);
+  rect(100,100,100,100);
+  line(100,100,200,200);
+}
