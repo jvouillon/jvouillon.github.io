f9e3e3faa4edbb34140fb0d052ab1bafbd778dce
jvouillon <jeanvouillon@mbp-de-jean.gateway.mts.net>
2017-03-02 13:55:13 -0600

    p5 photo

---
+++ CM127/temp/Photo/sketch.js
@@ -0,0 +1,24 @@
+var imgLoaded;
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  imageMode(CENTER);
+  imgLoaded = "";
+}
+
+function draw() {
+ background(200); 
+ //if (picURL){
+ //image(img, 0, 0);
+ if(picURL && !imgLoaded){
+  loadImage("leaf.png", function(img){imgLoaded=img;});
+  console.log("load image...");
+ }
+ 
+ if(imgLoaded){
+   image(imgLoaded,mouseX,mouseY);
+ }
+ 
+  ellipse(300,200,100);
+ //}
+}
