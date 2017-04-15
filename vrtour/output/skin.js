7d11d6d7b20e8e6f059051af5e314448314e0de0
jvouillon <jeanvouillon@mbp-de-jean.gateway.mts.net>
2017-04-15 16:28:59 -0500

    VR

--- vrtour/output/skin.js
+++ vrtour/output/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 5.1/15722
 // Filename: cardboard.ggsk
-// Generated sam. avr. 15 16:21:29 2017
+// Generated sam. avr. 15 16:27:37 2017
 
 function pano2vrSkin(player,base) {
 	var ggSkinVars = [];
