25f1ee57483f65017e645b2629e38648b69c5e67
jvouillon <jvouillon@ustboniface.ca>
2017-10-03 08:47:15 -0500

    text correction

--- CM215/00/sketch.js
+++ CM215/00/sketch.js
@@ -26,9 +26,9 @@ function draw() {
 noFill();
   for (var i = 0; i < touches.length; i++) {
     ellipse(touches[i].x, touches[i].y, touchSize, touchSize);
-    text(touches[i].x+20, touches[i].y, "# "+i);
-    text(touches[i].x+20, touches[i].y+10, " x: "+touches[i].x);
-    text(touches[i].x+20, touches[i].y+20, " y: "+touches[i].y);
+    text("#"+i, touches[i].x+20, touches[i].y);
+    text("x: "+touches[i].x, touches[i].x+20, touches[i].y+10);
+    text("y: "+touches[i].y, touches[i].x+20, touches[i].y+20);
     
     if ((i + 1) < touches.length) {
       line(touches[i].x, touches[i].y, touches[i + 1].x, touches[i + 1].y);
