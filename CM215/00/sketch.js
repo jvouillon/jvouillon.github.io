73cde04eb2fcbdd114bb831c237c53f9157d21c2
jvouillon <jvouillon@ustboniface.ca>
2017-10-03 08:41:36 -0500

    Touch 2017

--- CM215/00/sketch.js
+++ CM215/00/sketch.js
@@ -7,14 +7,15 @@ document.ontouchmove = function(event) {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ellipseMode(CENTER);
-  stroke(0);
-  strokeWeight(5);
-  touchSize = 100;
+  stroke(100);
+  strokeWeight(4);
+  touchSize = 60;
+  textSize = 20;
 }
 
 function draw() {
   background(255);
- fill(255, 0, 0);
+  fill(255, 0, 0);
   beginShape();
   for (var i = 0; i < touches.length; i++) {
     vertex(touches[i].x, touches[i].y);
@@ -25,7 +26,10 @@ function draw() {
 noFill();
   for (var i = 0; i < touches.length; i++) {
     ellipse(touches[i].x, touches[i].y, touchSize, touchSize);
-
+    text(touches[i].x+20, touches[i].y, "# "+i);
+    text(touches[i].x+20, touches[i].y+10, " x: "+touches[i].x);
+    text(touches[i].x+20, touches[i].y+20, " y: "+touches[i].y);
+    
     if ((i + 1) < touches.length) {
       line(touches[i].x, touches[i].y, touches[i + 1].x, touches[i + 1].y);
     } else {
